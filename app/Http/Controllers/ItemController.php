121c19045fbdc790d479f7799520a53d2778d927
Nabillah Umi Purnama <nabillahumi003@gmail.com>
2026-04-15 22:13:55 +0700

    ubah barang saya

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -32,38 +32,4 @@ class ItemController extends Controller
         return view('barang-saya', compact('items', 'total', 'tersedia', 'booking', 'terjual'));
     }
 
-    // // FORM TAMBAH
-    // public function create()
-    // {
-    //     return view('tambah-barang');
-    // }
-
-    //  // SIMPAN
-    // public function store(Request $request)
-    // {
-    //     $request->validate([
-    //         'name' => 'required',
-    //         'price' => 'required|numeric',
-    //         'location' => 'required',
-    //         'image' => 'nullable|image|mimes:jpg,jpeg,png|max:2048',
-    //     ]);
-
-    //     $imagePath = null;
-
-    //     if ($request->hasFile('image')) {
-    //         $imagePath = $request->file('image')->store('items', 'public');
-    //     }
-
-    //     Item::create([
-    //         'name' => $request->name,
-    //         'description' => $request->description,
-    //         'price' => $request->price,
-    //         'location' => $request->location,
-    //         'image' => $imagePath,
-    //         'condition' => 'Bekas',
-    //         'status' => 'tersedia',
-    //     ]);
-    //     return redirect()->route('barang.saya')->with('success', 'Barang berhasil ditambahkan!');
-    // }
-
 }
