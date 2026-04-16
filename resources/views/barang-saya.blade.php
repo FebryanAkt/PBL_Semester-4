fec12ab3ce6ae55662ee07617b9e4185968768fd
Faiva Puspa Sahara <puspafai5@gmail.com>
2026-04-16 19:39:49 +0700

    button

--- resources/views/barang-saya.blade.php
+++ resources/views/barang-saya.blade.php
@@ -82,7 +82,7 @@
 </div>
 
 {{-- FLOAT BUTTON --}}
-<a href="#"
+<a href="/edit-barang"
    class="fixed bottom-6 right-6 bg-yellow-300 w-14 h-14 flex items-center justify-center rounded-full shadow-lg text-2xl font-bold">
     +
 </a>
