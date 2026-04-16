ac66ac2247887015e3b724f383915e4bde7a5aaf
Nabillah Umi Purnama <nabillahumi003@gmail.com>
2026-04-16 19:29:18 +0700

    seeder user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::updateOrCreate(
+        ['email' => 'admin@gmail.com'], // Jika email sudah ada, update saja
+        [
+            'name' => 'Admin Bekaswit',
+            'password' => Hash::make('password123'), // Password standar buat tim
+        ]
+    );
+    }
+}
