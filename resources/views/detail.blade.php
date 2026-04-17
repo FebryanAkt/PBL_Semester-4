32fc6ba05006fffe32059eb1446b43cd4e17708d
Sweper27 <rifqiariessaputra@gmail.com>
2026-04-17 09:09:32 +0700

    penambahan gambar pada seeder

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -17,7 +17,9 @@
         {{-- Sisi Kiri: Gambar Produk --}}
         <div class="w-full md:w-1/2 bg-gray-50 p-6 md:p-10 flex items-center justify-center min-h-[300px] md:min-h-[500px]">
             @if($item->image)
-                <img src="{{ asset('storage/' . $item->image) }}" alt="{{ $item->name }}" class="object-contain w-full h-full max-h-[400px] drop-shadow-xl rounded-xl">
+                <img src="{{ asset('images/' . $item->image) }}" 
+                alt="{{ $item->name }}" 
+                class="w-full h-full object-contain">
             @else
                 <div class="text-center text-gray-400">
                     <svg class="w-24 h-24 mx-auto mb-4 opacity-50" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
