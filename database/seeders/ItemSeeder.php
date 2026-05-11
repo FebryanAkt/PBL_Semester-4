2c80b893891ff028685dbfffde2531a8ef3bf653
Sweper27 <rifqiariessaputra@gmail.com>
2026-05-11 12:02:08 +0700

    pearubahan pada item seeder

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -12,36 +12,6 @@ class ItemSeeder extends Seeder
     {
         $items = [
         [
-            'user_id' => 1, 
-            'name' => 'Laptop Axioo Hype 5 X5 (Minus Baterai Dikit)',
-            'price' => 3200000,
-            'location' => 'Lowokwaru',
-            'status' => 'tersedia',
-            'image' => 'laptop_axio.png',
-            'condition' => 'Bekas',
-            'description' => 'Dijual cepat laptop Axioo Hype 5 X5...'
-        ],
-        [
-            'user_id' => 1, 
-            'name' => 'Jaket Bomber Techwear WOLV American Canvas',
-            'price' => 185000,
-            'location' => 'Klojen',
-            'status' => 'tersedia',
-            'image' => 'jacket_bomber.png',
-            'condition' => 'Bekas',
-            'description' => 'Jaket gaya techwear bahan American Canvas tebal...'
-        ],
-        [
-            'user_id' => 1, 
-            'name' => 'Oximus Desk Mount Bracket Monitor',
-            'price' => 125000,
-            'location' => 'Blimbing',
-            'status' => 'terjual', 
-            'image' => 'bracket_monitor.png',
-            'condition' => 'Bekas',
-            'description' => 'Bracket monitor kokoh merk Oximus...'
-        ],
-        [
         'user_id' => 1,
         'name' => 'Jacket Bomber',
         'price' => 100000,
