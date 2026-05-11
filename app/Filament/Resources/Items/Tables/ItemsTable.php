60324f971bbe74fd9f1a01e5634a4f48cde798e9
Nabillah Umi Purnama <nabillahumi003@gmail.com>
2026-05-11 11:47:25 +0700

    menampilkan tabel items

--- app/Filament/Resources/Items/Tables/ItemsTable.php
+++ app/Filament/Resources/Items/Tables/ItemsTable.php
@@ -6,6 +6,7 @@ use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Tables\Table;
+use Filament\Tables;
 
 class ItemsTable
 {
@@ -13,7 +14,24 @@ class ItemsTable
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('id')
+                    ->label('ID')
+                    ->sortable(),
+
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Name')
+                    ->searchable(),
+
+                Tables\Columns\TextColumn::make('price')
+                    ->label('Price')
+                    ->numeric(),
+
+                Tables\Columns\TextColumn::make('stock')
+                    ->label('Stock'),
+
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label('Created At')
+                    ->dateTime(),
             ])
             ->filters([
                 //
